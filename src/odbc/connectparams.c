b8199a1b58d935ad9da4d529c58233baee16095b
Szilárd Parrag <szilard.parrag@oneidentity.com>
2022-05-11 13:46:05 +0200

    fixup! odbc/connectparams: add support for embedded "};"

--- src/odbc/connectparams.c
+++ src/odbc/connectparams.c
@@ -293,8 +293,10 @@ odbc_parse_connect_string_value_escaped(TDS_ERRS *errs, const char **current_pos
 	char *new_value_ptr;
 	char *new_value = strdup(*current_position);
 
-	if (!new_value)
-	  return false;
+	if (!new_value){
+	   odbc_errs_add(errs, "HY001", NULL);
+	   return false;
+	}
 
 	for (end = *current_position + 1, new_value_ptr = new_value  ; end != connect_string_end; ++end) {
 		char peek = (end+1) == connect_string_end ? '\0' : *(end+1);
